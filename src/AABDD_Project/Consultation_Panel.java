b146476b980c02258575b278ca05a224add6f271
mahdjourOussama <76874664+mahdjourOussama@users.noreply.github.com>
2022-01-14 14:53:00 +0100

    added refresh button

--- src/AABDD_Project/Consultation_Panel.java
+++ src/AABDD_Project/Consultation_Panel.java
@@ -33,6 +33,7 @@ public class Consultation_Panel extends javax.swing.JPanel {
         products_Table1 = new AABDD_Project.Products_Table();
         Searchfield = new javax.swing.JTextField();
         Table_Container = new javax.swing.JPanel();
+        jButton1 = new javax.swing.JButton();
 
         javax.swing.GroupLayout Table_ContainerLayout = new javax.swing.GroupLayout(Table_Container);
         Table_Container.setLayout(Table_ContainerLayout);
@@ -42,20 +43,27 @@ public class Consultation_Panel extends javax.swing.JPanel {
         );
         Table_ContainerLayout.setVerticalGroup(
             Table_ContainerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 278, Short.MAX_VALUE)
+            .addGap(0, 275, Short.MAX_VALUE)
         );
+
+        jButton1.setText("Refresh");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(Searchfield, javax.swing.GroupLayout.DEFAULT_SIZE, 452, Short.MAX_VALUE)
             .addComponent(Table_Container, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(Searchfield, javax.swing.GroupLayout.DEFAULT_SIZE, 143, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jButton1))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addComponent(Searchfield, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(Searchfield, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton1))
                 .addGap(18, 18, 18)
                 .addComponent(Table_Container, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
@@ -68,6 +76,7 @@ public class Consultation_Panel extends javax.swing.JPanel {
     private AABDD_Project.Departments_Table departments_Table1;
     private AABDD_Project.Employee_Table employee_Table1;
     private AABDD_Project.Fourniseur_Table fourniseur_Table1;
+    private javax.swing.JButton jButton1;
     private AABDD_Project.Products_Table products_Table1;
     // End of variables declaration//GEN-END:variables
 }
